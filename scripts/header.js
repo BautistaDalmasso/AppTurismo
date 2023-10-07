a91a1e97f5bca76018dc432a2246588aba95d9a5
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-10-07 18:54:32 -0300

    Agregar un header a todas las paginas.

---
+++ scripts/header.js
@@ -0,0 +1,18 @@
+const header = `
+<a class="nav-item" href="eventos.html">
+    <div>Eventos</div>
+</a>
+<a class="nav-item" href="sitios.html">
+    <div>Sitios y Atractivos</div>
+</a>
+<a class="nav-item" href="puntos_asistencia.html">
+    <div>Puntos de Asistencia</div>
+</a>
+<a class="nav-item" href="comercios.html">
+    <div>Comercios</div>
+</a>
+<a class="nav-item" href="productos.html">
+    <div>Productos</div>
+</a>`;
+
+document.getElementById("app-header").innerHTML = header;
