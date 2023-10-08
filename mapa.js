e859092dcd48bd591f0b6885ffe7a6476f4f7212
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-10-08 11:23:24 -0300

    Crear una maqueta inicial de la página.

---
+++ mapa.js
@@ -0,0 +1,10 @@
+
+function main() {
+    console.log("hola")
+    let map = L.map('map').setView([-34.5433, -58.7000], 13);
+    
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
+}
