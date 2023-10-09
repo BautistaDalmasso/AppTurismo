7cbda41fd3c9677ea81d667facda792daa9266b9
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-10-08 22:12:58 -0300

    Permite agregar eventos dinamicamente.

---
+++ scripts/handleEventos.js
@@ -0,0 +1,16 @@
+function agregarHTMLEventos() {
+    eventos.forEach(
+        evento => $("#cont-eventos").append(crearHTMLEvento(evento))
+    );
+}
+
+function crearHTMLEvento(evento) {
+    return `
+    <div id="evento-${evento.id}">
+        <h4>${evento.nombre}</h4>
+        <p>Descripción:</p>
+        <p>${evento.descripcion}</p>
+        <p>En: ${evento.lugar}</p>
+        <p>Fecha: ${evento.fecha} Hora: ${evento.hora}</p>
+    </div>`;
+}
