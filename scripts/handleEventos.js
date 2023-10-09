9b8b67ccbe600eb1226c409c7a5ef54a78a6a0e2
Bautista Dalmasso <j.bauti.d@gmail.com>
2023-10-09 18:37:26 -0300

    Agrega funcionalidad de filtrar.

--- scripts/handleEventos.js
+++ scripts/handleEventos.js
@@ -1,12 +1,29 @@
-function agregarHTMLEventos() {
-    eventos.forEach(
-        evento => $("#cont-eventos").append(crearHTMLEvento(evento))
+function filtrarEventos() {
+    let filtro = $("#search-event").val();
+    
+    let res = eventos.filter(
+        evento => evento.nombre.includes(filtro) || evento.descripcion.includes(filtro)
+    );
+    
+    manipularEventos(eventos, evento => $(`#${idEvento(evento)}`).hide());
+    manipularEventos(res, evento => $(`#${idEvento(evento)}`).show());
+}
+
+function manipularEventos(eventosAManipular, callback) {
+    eventosAManipular.forEach(
+        evento => callback(evento)
+    );
+}
+
+function agregarHTMLEventos(eventosAAgregar) {
+    eventosAAgregar.forEach(
+        evento => $("#result-eventos").append(crearHTMLEvento(evento))
     );
 }
 
 function crearHTMLEvento(evento) {
     return `
-    <div id="evento-${evento.id}" class="evento">
+    <div id="${idEvento(evento)}" class="evento">
         <h4>${evento.nombre}</h4>
         <p>Descripción:</p>
         <p>${evento.descripcion}</p>
@@ -15,4 +32,8 @@ function crearHTMLEvento(evento) {
         <img src="${evento.imagen}" alt="Imagen del evento">
         <hr>
     </div>`;
+}
+
+function idEvento(evento) {
+    return `evento-${evento.id}`;
 }
