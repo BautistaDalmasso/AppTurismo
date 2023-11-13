e8e151ec51dcb60c18279590b23ee1dc9cc405de
FranBarri <ftbarrientos103@gmail.com>
2023-11-13 12:54:35 -0300

    aniadidas fotos en comparar precios

--- Meli/meli.js
+++ Meli/meli.js
@@ -10,11 +10,35 @@ function buscarEnMercadoLibre() {
             const resultsDiv = document.getElementById('results');
             // Crear un listado de resultados
             const resultList = document.createElement('ul');
-            data.results.forEach(item => {
+
+            // Iterate through the first five items
+            for (let i = 0; i < 5; i++) {
+                const item = data.results[i];
                 const listItem = document.createElement('li');
-                listItem.textContent = item.title + ' - $' + item.price;
+                
+                // Create an image element
+                const image = document.createElement('img');
+                image.src = item.thumbnail; // Assuming 'thumbnail' contains the image URL
+
+                // Create a div to hold the image and details
+                const itemDetails = document.createElement('div');
+
+                // Create elements for title and price
+                const title = document.createElement('h3');
+                title.textContent = item.title;
+
+                const price = document.createElement('p');
+                price.textContent = 'Price: $' + item.price;
+
+                // Append elements to the list item
+                itemDetails.appendChild(title);
+                itemDetails.appendChild(price);
+                listItem.appendChild(image);
+                listItem.appendChild(itemDetails);
+
                 resultList.appendChild(listItem);
-            });
+            }
+
             // Mostrar los resultados en la página
             resultsDiv.innerHTML = ''; // Limpiar resultados anteriores
             resultsDiv.appendChild(resultList);
